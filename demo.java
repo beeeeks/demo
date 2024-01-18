924d37123537d3d79cc1567ea4d537009aa83129
Bikram Parki <bikramparki48@gmail.com>
2024-01-18 21:34:05 +0545

    second commit

---
+++ demo.java
@@ -0,0 +1,11 @@
+public class demo {
+    public static void main(String[] args) {
+        // Printing to the console without a new line
+        System.out.print("Hello, ");
+        System.out.print("World!");
+
+        // Printing to the console with a new line
+        System.out.println("\nHello, World!");
+    }
+    
+}
